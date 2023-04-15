8aa6028e313cb5418ef4c15eccb5f902b0e4d911
Advait Soni <advaitsoni@pop-os.localdomain>
2023-04-15 16:55:26 +0530

    adv

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ let myLeads = []
 let ulEl = document.getElementById("ul-el")
 
 let inputEl = document.getElementById("input-el")
+let deleteBtn = document.getElementById("delete-btn")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+deleteBtn.addEventListener("click", function(){
+    myleads = []
+    localStorage.clear()
+    ulEl.innerHTML = ""
+})
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
     inputEl.value = ""
     renderLeads()
 })
